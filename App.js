cd192b3817d009d41d26208645489b02dc9ab47d
SeolShin <39841236+SeolShin@users.noreply.github.com>
2018-06-11 13:50:01 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+//import {flowers} from './flowers';
+import {robots} from './robots';
