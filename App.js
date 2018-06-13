c1161e658e812cae04df6d611a1ff5c4e259db0d
SeolShin <39841236+SeolShin@users.noreply.github.com>
2018-06-13 13:19:40 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+    Component
+} from 'react';
 import './App.css';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-//import {flowers} from './flowers';
-import {robots} from './robots';
+import CardList from './containers/CardList';
+import SearchBox from './containers/SearchBox';
+import 'tachyons'
+import Scroll from './containers/Scroll';
+
+import {
+    fList
+} from './fList';
+
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            fList: [],
+            searchfield: ``
+        }
+    } //constructor
+
+    onSearchChange = (event) => {
+
+        this.setState({
+            searchfield: event.target.value
+        })
+
+    } //onSearchChange
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+            .then(response => {
+                return response.json();
+            })
+            .then(photos => this.setState({
+                fList: photos
+            }));
+    }
+
+    render() {
+        const {
+            fList,
+            searchfield
+        } = this.state;
+
+        const filteredRobots = fList.filter(fList => {
+            return fList.title.toLowerCase().includes(searchfield.toLowerCase())
+        }) //filteredTarget //filter
+
+        return !fList.length ?
+            <
+            h1 > Loading < /h1> :
+            ( <
+                div className = 'tc' >
+                <
+                h1 className = 'f1' > CollectFlowers < /h1> <
+                SearchBox searchChange = {
+                    this.onSearchChange
+                }
+                />  <
+                Scroll >
+                <
+                CardList fList = {
+                    filteredRobots
+                }
+                />  < /
+                Scroll > < /
+                div >
+            )
+    } //render
+}; //class
+
+export default App;
